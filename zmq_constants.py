93c895548b471289a3bc4dab6ad8b3070cd881e6
Federico Ferri <federico.ferri.it@gmail.com>
2021-03-09 18:39:00 +0100

    skip some ZMQ_* symbols

--- zmq_constants.py
+++ zmq_constants.py
@@ -20,15 +20,20 @@ for d in args.zmq_dirs:
 if not zmq_h:
     raise Exception('zmq.h not found in any of the specified locations')
 
-print('found zmq.h at %s' % zmq_h)
+def skip(sym):
+    if sym == 'EXPORT': return True
+    if re.match(r'DEFINED_\w+', sym): return True
+    if re.match(r'VERSION(|_MAJOR|_MINOR|_PATCH)', sym): return True
 
 with open(args.out_file, 'w') as f_out:
     f_out.write('#undef __ZMQ_H_INCLUDED__\n')
     with open(zmq_h, 'r') as f_in:
         for line in f_in:
             if m := re.match('#define\s+ZMQ_(\w+)\s+([^\s]+)', line):
-                f_out.write(f'sim::registerScriptVariable("simZMQ.{m.group(1)}", ZMQ_{m.group(1)}, 0);')
-                f_out.write('\n')
+                sym = m.group(1)
+                if not skip(sym):
+                    f_out.write(f'sim::registerScriptVariable("simZMQ.{sym}", ZMQ_{sym}, 0);')
+                    f_out.write('\n')
             elif m := re.match('#if.*', line):
                 f_out.write(line + '\n')
             elif m := re.match('#endif.*', line):
